2e172299ef820fe0f7e984c2dde97a553bd67985
arkaanni <arkaannuruliman@gmail.com>
2021-08-30 19:05:57 +0700

    remove websocket dependency

--- src/components/Position.svelte
+++ src/components/Position.svelte
@@ -1,24 +1,26 @@
 <script>
   import { mainnet_usdt_endpoint } from '../config/config.js'
   import { auth } from '../config/auth.js'
-  import WebSocket from 'ws'
-
+  import { onMount } from 'svelte';
   let position = ''
-  const ws = new WebSocket(mainnet_usdt_endpoint)
-  ws.onopen = (event) => {
-    auth(ws)
-    ws.send(JSON.stringify({"op": "subscribe", "args": ["position"]}))
-  }
-  ws.onmessage = (event) => {
-    try {
-      const data = JSON.parse(event.data)
-      if (data.topic === "position") {
-        console.log(data.data)
-      }
-    } catch (e) {
-      console.log(e)
+  onMount(() => { 
+    const ws = new WebSocket(mainnet_usdt_endpoint)
+    ws.onopen = (event) => {
+      auth(ws)
+      ws.send(JSON.stringify({"op": "subscribe", "args": ["position"]}))
     }
-  }
+    ws.onmessage = (event) => {
+      try {
+        const res = JSON.parse(event.data)
+        if (res.topic === "position") {
+          console.log(res.data)
+        }
+      } catch (e) {
+        console.log(e)
+      }
+    }
+  })
+
 </script>
 
 <div>{position}</div>

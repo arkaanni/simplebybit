563a7dec3afb6df132e01d9f88dfd5b65440f0aa
arkaanni <arkaannuruliman@gmail.com>
2021-08-31 06:27:08 +0700

    endpoints

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,10 @@
-export const testnet_endpoint = import.meta.env.VITE_PUBLIC_TESTNET_ENDPOINT
-export const mainnet_endpoint = import.meta.env.VITE_PUBLIC_MAINNET_ENDPOINT
-export const mainnet_usdt_endpoint = import.meta.env.VITE_PUBLIC_MAINNET_USDT_ENDPOINT
-export const api_key = import.meta.env.VITE_API_KEY
-export const api_secret = import.meta.env.VITE_API_SECRET
+const mainnet_inverse_ws = "wss://stream.bytick.com/realtime"
+const mainnet_usdt_ws = "wss://stream.bytick.com/realtime_public"
+const mainnet_usdt_private_ws = "wss://stream.bytick.com/realtime_private"
+const mainnet_restapi = "https://api.bytick.com"
+const api_key = import.meta.env.VITE_API_KEY
+const api_secret = import.meta.env.VITE_API_SECRET
+
+export {
+  mainnet_restapi, mainnet_inverse_ws, mainnet_usdt_ws, mainnet_usdt_private_ws, api_key, api_secret
+}
